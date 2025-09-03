346791e80fc0ee6d9153605fe55e9f122fcd47c6
Jabez Ayson <aysonjabezm@gmail.com>
2025-09-03 23:58:56 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    let timers = $state([
+        { id: 1, name: "1 Minute", duration: 60, isRunning: false, end: 65435453445 },
+        { id: 2, name: "2 Minutes", duration: 120, isRunning: false, end: 65435453445 }
+    ]);
+
+    let answer : string = $state('');
+    // let selected : string = $state();
+
+    function addTimer(e: Event) {
+        e.preventDefault();
+
+		// alert(
+		// 	`answered question ${selected.id} (${selected.name}) with "${answer}"`
+		// );
+    }
+
+</script>
+
+<h1>Synced Timer</h1>
+
+<form onsubmit={addTimer}>
+    <label for="timer">Add Timer</label>
+    <input bind:value={answer} />
+
+    <button type="submit">Add</button>
+
+
+</form>
